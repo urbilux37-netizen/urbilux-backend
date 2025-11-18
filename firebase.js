aed3aced814106bd9db8b430055d8957620beb87
highrohan69-lab <high.rohan69@gmail.com>
2025-11-18 09:57:51 +0600

    Remove local firebase json file

--- firebase.js
+++ firebase.js
@@ -1,8 +1,22 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service.json");
+
+const projectId = process.env.FIREBASE_PROJECT_ID;
+const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n");
+
+if (!projectId || !clientEmail || !privateKey) {
+  console.error("❌ Firebase ENV Missing! Check Render environment variables.");
+  process.exit(1);
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId,
+    clientEmail,
+    privateKey,
+  }),
 });
+
+console.log("🔥 Firebase Admin Initialized Successfully");
 
 module.exports = admin;
